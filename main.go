9cefd3d79081433dcce43e379f758e8fed2686e8
Venkatesh Gaddam <vgaddam@csod.com>
2022-10-12 19:39:25 +0530

    Added First Lambda Function Code in GoLang

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"What is your Name?"`
+	Age  int32  `json:"Your Age"`
+}
+
+type MyResponse struct {
+	Message string `json:"Answer"`
+}
+
+func Handler(event MyEvent) (MyResponse, error) {
+	result := fmt.Sprintf("%s is %d years old!", event.Name, event.Age)
+	fmt.Println(result)
+	return MyResponse{Message: result}, nil
+}
+
+func main() {
+
+	fmt.Println("This is working in Serverless!!!")
+	lambda.Start(Handler)
+}
